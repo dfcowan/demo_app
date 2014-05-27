75eadcc58c2968e2d9d76a4a332cbcb3c0242543
Drew Cowan <dcowan@labattfood.com>
2014-05-26 21:30:28 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '718afc2c87bff7f62edc5244c7060bedd0d2653091faedc29ebdc93d26461598d0f6a9ec3c5d469d173f3e5124997b4634e6c5b97e24950d589779c006764ee9'
